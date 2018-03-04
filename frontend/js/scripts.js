52731bfd100996bc9af0c16cfb300a90d24799cc
MrPatel95 <jeetpatel13@live.com>
2018-03-04 00:59:08 -0500

    Received all restaurants from DB

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -47,7 +47,6 @@ function createRequestObject(){
 
 //  This function is called when the search button is clicked
 function searchRestaurants(){
-    alert("Entered function");
     
     var state = document.getElementById("state_id").value;
     var city = document.getElementById("city_id").value;
@@ -56,7 +55,21 @@ function searchRestaurants(){
     var httpReq = createRequestObject();
     httpReq.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
-            console.log(this.responseText);
+            alert("Got message");
+            var response = JSON.parse(this.responseText);
+            
+            var restaurants = response['restaurants'];
+            
+            for(var i in restaurants){
+                console.log(restaurants[i].restaurant_id);
+                console.log(restaurants[i].name);
+                console.log(restaurants[i].menu_url);
+                console.log(restaurants[i].thumb);
+                console.log(restaurants[i].address);
+                console.log(restaurants[i].city_id);
+                console.log(restaurants[i].rating);
+                console.log(restaurants[i].rating_text);
+            }
         }
     }
     httpReq.open("GET", "../php/getAllRestaurants.php?state="+state+"&city="+city+"&cuisine_id="+cuisine_id, true);
